ff9bb40317cfb2992eaa5720c2c85c11147d7566
Cactus-2 <1romerolonic@hdsb.ca>
2025-03-31 11:09:57 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,9 +2,9 @@
 # Name:        Assignment Template (assignment.py)
 # Purpose:     A description of your program goes here.
 #
-# Author:      Mr. Brooks
-# Created:     13-Sept-2020
-# Updated:     13-Sept-2020
+# Author:      Nicolas
+# Created:     31-march-2025
+# Updated:     13-Sept-2025
 #-----------------------------------------------------------------------------
 #I think this project deserves a level XXXXXX because ...
 #
@@ -13,3 +13,15 @@
 #   ...
 #   ...
 #-----------------------------------------------------------------------------
+
+import pygame
+
+pygame.init()
+pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Game")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.QUIT()
+    pygame.display.update()
